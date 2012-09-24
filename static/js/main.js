6cedd9c896dada3d1700d682a19416bf5e0c652e
Joe Gibson <joseph.gibson@students.olin.edu>
2012-09-24 18:05:39 -0400

    starting main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,17 @@
+function on_load() {
+	
+	
+	$(window).scroll(function(){
+		if  ($(window).scrollTop() >= 229){
+			$('#main_table').css({position:'relative',top:46});
+		} else {
+			$('#main_table').css({top:0});
+        }
+        
+        if  ($(window).scrollTop() >= 229) {
+			$('#navigation_table').css({position:'fixed',top:0,left:'50%',margin:'0 0 0 -487.5px'});
+		}
+	});
+}
 
+//$(document).ready(on_load)
