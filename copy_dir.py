f795a4952693ba72eb04290125a4f268fd241707
Joe Gibson <joseph.gibson@students.olin.edu>
2012-09-25 17:36:11 -0400

    organized images

--- copy_dir.py
+++ copy_dir.py
@@ -1,5 +1,28 @@
 import os
+import re
 
-cd = "cd /var/www/virtual/hfid.olin.edu/html/
+loc = "/var/www/virtual/hfid.olin.edu/html/sa2013/s_engr3220-fallera"
+cmd = "scp"
 
-def 
+cwd = ""
+
+filePattern = ".html|.jpg|.JPG|.png|.css|.js"
+folderPattern = "\."
+
+def nav_folder(path = ""):
+	if path != cwd:
+		cwd = path
+		
+	directory = os.popen("ls")
+	
+	for line in directory.readlines():
+		dirObj = line[0:-1]
+		files = re.findall(filePattern, dirObj)
+		if len(files):
+			print dirObj, "FILE"
+		folders = re.findall(folderPattern, dirObj)
+		if not len(folders):
+			print dirObj, "FOLDER"
+		
+		
+nav_folder()
