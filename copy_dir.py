13a34ae4d96ab61d5c014c852b63ae7c018896db
Joe Gibson <joseph.gibson@students.olin.edu>
2012-09-25 22:09:24 -0400

    copying script almost done

--- copy_dir.py
+++ copy_dir.py
@@ -1,14 +1,15 @@
 import os
 import re
+import time
+import paramiko
 
 loc = "/var/www/virtual/hfid.olin.edu/html/sa2013/s_engr3220-fallera"
 cmd = "scp"
+server = "jgibson@linwebprod.olin.edu:"
 
 filePattern = ".html|.jpg|.JPG|.png|.css|.js"
 folderPattern = "\."
 myDirPattern = "/home/jgibson/git/HFID_team_site"
-
-queue = []
 
 def nav_folder(path = ""):
 	cwd = os.popen("pwd")
@@ -35,7 +36,18 @@ def nav_folder(path = ""):
 	r = "/".join(r)
 	os.chdir(r)
 		
-		
-nav_folder()
+def make_copy(fileName = "", path = ""):
+	c = cmd + " " + fileName + " " + server + loc + "/test" + fileName
+	print c
+	t = os.popen(c)
+	for line in t.readlines():
+		print line
+	os.system(c)
+	
+	
+make_copy("index.html")
+	
+	
+#nav_folder()
 
 
